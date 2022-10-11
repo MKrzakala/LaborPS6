f7e577dd1a90324596fed77e6f89ad16e01b7533
MKrzakala <52800131+MKrzakala@users.noreply.github.com>
2022-10-11 20:35:50 +0200

    Update ex1.jl

--- ex1.jl
+++ ex1.jl
@@ -22,3 +22,22 @@ for t in 1:66
 end
 sum(mu[:])
 plot(1:67,mu[:])
+mu_1=deepcopy(mu)
+
+#now for different n:
+n=0.01
+mu[1]=1
+for t in 1:66
+    mu[t+1]=mu[t]*(1-df1[(25+t),2])/(1+n)
+end
+sum_mu=sum(mu[:])
+mu[1]=1/sum_mu
+for t in 1:66
+    mu[t+1]=mu[t]*(1-df1[(25+t),2])/(1+n)
+end
+sum(mu[:])
+mu_2=deepcopy(mu)
+
+plot(1:67,mu_1[:], label="n=0.02")
+plot!(1:67,mu_2[:], label="n=0.01")
+
